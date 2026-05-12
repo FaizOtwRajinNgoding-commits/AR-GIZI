7cc8f43dcf71b617a1bf56077a8a9725cd6628ec
FaizOtwRajinNgoding-commits <faiz07346@gmail.com>
2026-05-12 17:02:41 +0700

    MainMenu and AR

---
+++ Assets/Scripts/UI/AppManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AppManager : MonoBehaviour
+{
+	public string ARScene;
+
+    public string GameScene;
+
+
+    public void MulaiAR()
+        {
+            SceneManager.LoadScene(ARScene);
+        }
+
+    public void MulaiGame()
+    {
+        SceneManager.LoadScene(GameScene);
+    }
+	
+	public void KeluarAplikasi()
+	{
+		Application.Quit();
+	}
+
+}
