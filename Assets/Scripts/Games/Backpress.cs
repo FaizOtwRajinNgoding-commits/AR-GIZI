0d2539f4e37ec5bfcbbd8223eca2e3dd889eabab
Muhammad Shifan Raihan <shifanraihan@gmail.com>
2026-05-12 16:50:50 +0700

    Create Basic GameMenu UI
    Layout Minimal untuk GameScene + popup confirm backbutton kasar untuk integrasi ke MainMenu

---
+++ Assets/Scripts/Games/Backpress.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Backpress : MonoBehaviour {
+
+	// berfungsi untuk keluar aplikasi menggunakan tombol back
+	public string SceneName;
+	public GameObject popupPanel;
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Escape))
+        {
+			SceneManager.LoadScene(SceneName);
+        }
+	}
+
+	// Dipanggil saat tombol Back ditekan
+	public void ShowPopup()
+    {
+        popupPanel.SetActive(true);
+    }
+
+    // Tombol "Tidak"
+    public void CancelExit()
+    {
+        popupPanel.SetActive(false);
+    }
+
+    // Tombol "Iya"
+    public void ConfirmExit()
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+}
